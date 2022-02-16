61ded4827db58987cb3f98bcc3b5b8c2ee4be4fc
Jun Ming Ooi <ojmjunming@gmail.com>
2022-02-16 15:26:41 +0400

    add pills n movie palyback

---
+++ Assets/Video/VideoController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public GameObject VideoPlayer;
+    public GameObject VideoOutput;
+    public bool isPlayerStarted = false;
+
+    void Start() {
+
+    }
+
+    void Update()
+    {
+      var video = VideoPlayer.GetComponent<VideoPlayer>();
+      if (video) {
+        Debug.Log(isPlayerStarted + " hello " + video.isPlaying);
+        if (isPlayerStarted == false && video.isPlaying == true) {
+            // When the player is started, set this information
+            isPlayerStarted = true;
+        }
+        if (isPlayerStarted == true && video.isPlaying == false ) {
+            // Wehen the player stopped playing, hide it
+            video.gameObject.SetActive(false);
+            VideoOutput.gameObject.SetActive(false);
+        }
+      }
+    }
+}
