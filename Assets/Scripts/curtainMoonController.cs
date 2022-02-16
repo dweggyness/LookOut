e38306da162da53d81a2c4662bf2627ff34a2e7d
Jun Ming Ooi <ojmjunming@gmail.com>
2022-02-16 22:41:22 +0400

    Finito

---
+++ Assets/Scripts/curtainMoonController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class curtainMoonController : MonoBehaviour
+{
+  public GameObject curtain1;
+  public GameObject curtain2;
+  public GameObject curtain3;
+  public Animator anim1;
+  public Animator anim2;
+  public Animator anim3;
+
+  public bool hasCurtain1Opened = false;
+  public bool hasCurtain2Opened = false;
+  public bool hasCurtain3Opened = false;
+  public float moonlightIntensity = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim1 = curtain1.GetComponent<Animator>();
+        anim2 = curtain2.GetComponent<Animator>();
+        anim3 = curtain3.GetComponent<Animator>();
+
+        moonlightIntensity = RenderSettings.ambientIntensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        // video lasts for 140 seconds, knocking ends at 149~ seconds. we open first window
+        // at 152 seconds.
+        // anim time of each curtain:
+        // curtain 1 : 4s
+        // curtain 2 : 4s
+        // curtain 3 : 6s
+        if (Time.time >= 152 && !hasCurtain1Opened) {
+          hasCurtain1Opened = true;
+          anim1.SetTrigger("openCurtain");
+        }
+        
+        if (Time.time >= 158  && !hasCurtain2Opened) {
+          hasCurtain2Opened = true;
+          anim2.SetTrigger("openCurtain");
+        }
+        
+        if (Time.time >= 166 && !hasCurtain3Opened) {
+          hasCurtain3Opened = true;
+          anim3.SetTrigger("openCurtain");
+        }
+        
+        moonlightIntensity += 0.001f;
+
+        if (hasCurtain2Opened) {
+          moonlightIntensity += 0.001f;
+        }
+
+       RenderSettings.ambientIntensity = moonlightIntensity;
+    }
+}
